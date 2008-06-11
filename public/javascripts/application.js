e135c19ac30fde92638fb79d6b1cd5bb9654be2d
M. Auayan <sxtxixtxcxh@gmail.com>
2008-06-11 05:19:44 +0000

    updated form, updated images, added copy/paste functionality to preview.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,35 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function clear_default_value(field,defaultValue){
+  if(field.value == defaultValue){
+    field.value = '';
+    $(field).removeClassName('fieldDefault');
+  }
+}
+
+function reset_default_value(field,defaultValue){
+  if(field.value == ''){
+    field.value = defaultValue;
+    $(field.id).addClassName('fieldDefault');
+    
+  }
+}
+
+function copy(inElement) {
+  if (inElement.createTextRange) {
+    var range = inElement.createTextRange();
+    if (range && BodyLoaded==1)
+      range.execCommand('Copy');
+  } else {
+    var flashcopier = 'flashcopier';
+    if(!document.getElementById(flashcopier)) {
+      var divholder = document.createElement('div');
+      divholder.id = flashcopier;
+      document.body.appendChild(divholder);
+    }
+    document.getElementById(flashcopier).innerHTML = '';
+    var divinfo = '<embed src="/images/_clipboard.swf" FlashVars="clipboard='+encodeURIComponent(inElement.value)+'" width="0" height="0" type="application/x-shockwave-flash"></embed>';
+    document.getElementById(flashcopier).innerHTML = divinfo;
+  }
+}
